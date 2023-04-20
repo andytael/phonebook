c238c62876ec95e9ae9a2c1a291cc548049bd08b
Andy Tael <andy.tael@yahoo.com>
2023-04-20 16:53:58 -0500

    Initial commit

--- src/main/java/com/example/phonebook/PhonebookApplication.java
+++ src/main/java/com/example/phonebook/PhonebookApplication.java
@@ -2,12 +2,16 @@ package com.example.phonebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PhonebookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhonebookApplication.class, args);
     }
 
+    // add Eureka and actuator stuff
 }
